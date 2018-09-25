dcd83f012d284088ec57c0c24f1c504a1babe082
E11ie <jessieadrian3@gmail.com>
2018-09-24 19:19:01 -0500

    added templates and todos

--- main.py
+++ main.py
@@ -1,71 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# page header & footer
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>User Signup</title>
-    </head>
-    <body>
-        <h1>User Signup</h1>
-"""
 
-page_footer = """
-    </body>
-</html>
-"""
-# TODO: inputs for username, password, verify password, and email (optional) and submit button
-user_signup_form = """
-<form method="post">
-	<table>
-		<tr>
-			<td>
-				<label for="username">Username</label>
-			</td>
-			<td>
-				<input name="username" type="text" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<label for="password">Password</label>
-			</td>
-			<td>
-				<input name="password" type="password" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<label for="verify-password">Verify Password</label>
-			</td>
-			<td>
-				<input name="verify-password" type="password" />
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<label for="email">Email (optional)</label>
-			</td>
-			<td>
-				<input name="email" type="text" />
-			</td>
-		</tr>
-	</table>
-	<input type="submit" value="Submit" />
-</form>
-"""
+# TODO
+# if submission of signup form is not valid, reject and re-render form with feedback of what they did wrong
 
-# Build Page by sandwiching content with page header and footer
-def build_page_content(content_string):
-	return page_header + content_string + page_footer
+# TODO
+# signup form is not valid if:
+#	these fields are empty: username, password, verify password
+#	username and/or is not valid: contains a space, less than 3 characters or more than 20
+#	password and verify password do not match
+#	if email is not blank, but is not a valid email: has one '@' and one '.', no spaces, between 3 and 20 
+#		characters long
+
+# TODO
+# each feedback error should be next to the field it refers to
+
+# TODO
+# if there is an error, keep the username and email fields, and clear the password fields
+
+# TODO
+# if submission of signup form is valid, display a welcome page: "Welcome, [username]!"
+
+# TODO	
+# use templates (one for home page and one for the welcome page) to render the html
+
 
 @app.route('/')
 def index():
-	return build_page_content(user_signup_form)
+	return render_template('signup_form.html')
 
 
 app.run()
