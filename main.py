d1d8c03a25c5e62b39d83e44b6c3dc315cd0b785
E11ie <jessieadrian3@gmail.com>
2018-09-24 19:47:51 -0500

    added welcome template

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.config['DEBUG'] = True
 # TODO
 # signup form is not valid if:
 #	these fields are empty: username, password, verify password
-#	username and/or is not valid: contains a space, less than 3 characters or more than 20
+#	username and/or password is not valid if: contains a space, less than 3 characters or more than 20
 #	password and verify password do not match
 #	if email is not blank, but is not a valid email: has one '@' and one '.', no spaces, between 3 and 20 
 #		characters long
@@ -32,5 +32,41 @@ app.config['DEBUG'] = True
 def index():
 	return render_template('signup_form.html')
 
+
+@app.route("/signup", methods=['POST'])
+def signup():
+	# look inside the request to figure out what the user typed
+	username = request.form['username']
+	password = request.form['password']
+	verify_password = request.form['verify-password']
+	email = request.form['email']
+
+	# types of errors
+	error = ''
+	error_username = ''
+	error_password = ''
+	error_verify_password = ''
+	error_email = ''
+
+	# these fields are empty: username, password, verify password
+
+	# check if username and/or password is valid 
+
+	# check if email is valid or empty
+
+	# if all checks passed, then render the welcome page
+	return render_template('welcome.html', username=username)
+
+
+def valid_username(string):
+	# TODO
+	# returns False if it contains a space, or less than 3 characters or more than 20
+	return False
+
+def valid_email(string):
+	# TODO
+	# A valid email: has one '@' and one '.', no spaces, between 3 and 20 
+	# characters long
+	return False
 
 app.run()
