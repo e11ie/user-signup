3c45e7b498eb69ce3f15c37fa0ed1de0b07f4db1
E11ie <jessieadrian3@gmail.com>
2018-10-01 18:48:02 -0500

    added validation to input fields, added error messages and variables to templates

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 from flask import Flask, request, redirect, render_template
+import cgi
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-# TODO
-# if submission of signup form is not valid, reject and re-render form with feedback of what they did wrong
-
-# TODO
-# signup form is not valid if:
-#	these fields are empty: username, password, verify password
-#	username and/or password is not valid if: contains a space, less than 3 characters or more than 20
-#	password and verify password do not match
-#	if email is not blank, but is not a valid email: has one '@' and one '.', no spaces, between 3 and 20 
-#		characters long
-
-# TODO
-# each feedback error should be next to the field it refers to
-
-# TODO
-# if there is an error, keep the username and email fields, and clear the password fields
-
-# TODO
-# if submission of signup form is valid, display a welcome page: "Welcome, [username]!"
-
-# TODO	
-# use templates (one for home page and one for the welcome page) to render the html
-
-
 @app.route('/')
 def index():
-	return render_template('signup_form.html')
+	error_username = request.args.get("error_username")
+	error_password = request.args.get("error_password")
+	error_verify_password = request.args.get("error_verify_password")
+	error_email = request.args.get("error_email")
+	error = request.args.get("error")
+	username = request.args.get("username")
+	email = request.args.get("email")
+	return render_template('signup_form.html',
+		error_username=error_username and cgi.escape(error_username, quote=True),
+		error_password=error_password and cgi.escape(error_password, quote=True),
+		error_verify_password=error_verify_password and cgi.escape(error_verify_password, quote=True),
+		error_email=error_email and cgi.escape(error_email, quote=True),
+		error=error and cgi.escape(error, quote=True),
+		username=username and cgi.escape(username, quote=True),
+		email=email and cgi.escape(email, quote=True))
 
 
 @app.route("/signup", methods=['POST'])
@@ -48,25 +40,88 @@ def signup():
 	error_verify_password = ''
 	error_email = ''
 
-	# these fields are empty: username, password, verify password
+	# check to see if these fields are empty: username, password, verify password
+	if username == '':
+		error_username = 'Please enter a username.'
 
-	# check if username and/or password is valid 
+	if password == '':
+		error_password = 'Please enter a password.'
+
+	if verify_password == '':
+		error_verify_password = 'Please verify your password.'
+
+	if username == '' or password == '' or verify_password == '':
+		error= 'Please complete the required fields'
+		return redirect('/?error=' + error +
+			'&error_username=' + error_username +
+			'&error_password=' + error_password +
+			'&error_verify_password=' + error_verify_password +
+			'&username=' + username +
+			'&email=' + email)
+
+	# check if username is valid 
+	if not valid_len_and_char(username):
+		error_username = 'Please enter a valid username.'
+		error = 'Note: only characters a-z, A-Z, and 0-9, and between 3 and 20 characters in length.'
+		return redirect('/?error=' + error +
+			'&error_username=' + error_username +
+			'&username=' + username +
+			'&email=' + email)
+
+	# check if password is valid 
+	if not valid_len_and_char(password):
+		error_password = 'Please enter a valid password.'
+		error = 'Note: only characters a-z, A-Z, and 0-9, and between 3 and 20 characters in length.'
+		return redirect('/?error=' + error +
+			'&error_username=' + error_password +
+			'&username=' + username +
+			'&email=' + email)
+
+	# check that password and verify_password match
+	if password != verify_password:
+		error_verify_password = 'Does not match the password.'
+		error = 'Please make sure the password matched the verify password field.'
+		return redirect('/?error=' + error +
+			'&error_verify_password=' + error_verify_password +
+			'&username=' + username +
+			'&email=' + email)
 
 	# check if email is valid or empty
+	if email != '' and not valid_email(email):
+		error_email = 'Please enter a correct email.'
+		error = 'Note: No spaces, between 3 and 20 characters, and have one "@" and one "." character'
+		return redirect('/?error=' + error +
+			'&error_email=' + error_email +
+			'&username=' + username +
+			'&email=' + email)
 
 	# if all checks passed, then render the welcome page
 	return render_template('welcome.html', username=username)
 
+# re.fullmatch
+def valid_len_and_char(string):
 
-def valid_username(string):
-	# TODO
-	# returns False if it contains a space, or less than 3 characters or more than 20
-	return False
+	# Must be a between 3 and 20 characters in length
+	if len(string) < 3 or len(string) > 20:
+		return False
+	# Must have only alphanumeric characters
+	if re.search(r'\W|_', string) != None:
+		return False
+
+	return True
 
 def valid_email(string):
-	# TODO
-	# A valid email: has one '@' and one '.', no spaces, between 3 and 20 
-	# characters long
-	return False
+
+	# Must be a between 3 and 20 characters in length
+	if len(string) < 3 or len(string) > 20:
+		return False
+
+	# Must have no spaces
+	# A valid email: has one '@' and one '.'
+	regex = re.compile(r'^([^\.@\s])+@[^\.@\s]+\.[^\.@\s]+$')
+	if regex.search(string) == None :
+		return False
+
+	return True
 
 app.run()
